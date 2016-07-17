f692895cc7186c6c55e380751c04fd43c0b87fd7
Muthu <muthu.comp@yahoo.com>
2016-07-17 23:10:07 +0530

    Initial Commit Line drawing

---
+++ Line draw Project solution/Test Appln/Mytextbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Appln
+{
+    public class Mytextbox : TextBox
+    {
+        public Mytextbox()
+        {
+            this.Text = "myxty";
+            this.Enabled = false;
+        }
+    }
+}
